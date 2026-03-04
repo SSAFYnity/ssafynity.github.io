42b12eea135304cd1c1df712e3ae669360121355
hadevyi <hadevyi@gmail.com>
2026-03-04 09:15:15 +0900

    페이지 리랜더링 시 positionY 재배치 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import HomePage from '@/pages/index'
@@ -29,6 +36,7 @@ import NotFoundPage from '@/pages/not-found'
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-1">
